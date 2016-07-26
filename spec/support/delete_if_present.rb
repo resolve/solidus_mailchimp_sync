7782700989f1bd8005090db5a53a145cc7161501
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-07-26 15:03:38 -0400

    remove debugger

--- spec/support/delete_if_present.rb
+++ spec/support/delete_if_present.rb
@@ -1,7 +1,6 @@
 def delete_if_present(mailchimp_api_url)
   response = SolidusMailchimpSync::Mailchimp.ecommerce_request(:delete, mailchimp_api_url, return_errors: true)
   if response.kind_of?(SolidusMailchimpSync::Error) && response.status != 404
-    byebug
     raise response
   end
   response
