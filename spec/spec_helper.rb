8f8628bfcb8992ed1cd2c83d6dbc31dd9244d81f
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-07-26 15:11:26 -0400

    fix VCR setup to really work without api key and store id set

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,14 @@ VCR.configure do |config|
 
   # Filter out basic auth
   config.filter_sensitive_data('<OMITTED AUTH HEADER>') { Base64.strict_encode64("#{SolidusMailchimpSync::Mailchimp::AUTH_USER}:#{SolidusMailchimpSync.api_key}") }
+
+  # Filter store_id and api key, and set them to defaults so code won't complain about missing
+  # arguments.
+  SolidusMailchimpSync.store_id ||= "dummy-store-id"
+  SolidusMailchimpSync.api_key ||= 'dummy-api-key-us1'
+  config.filter_sensitive_data('dummy-store-id') { SolidusMailchimpSync.store_id }
+  config.filter_sensitive_data("dummy-api-key") { SolidusMailchimpSync.api_key }
+  config.filter_sensitive_data("us1") { SolidusMailchimpSync.data_center }
 end
 
 RSpec.configure do |config|
