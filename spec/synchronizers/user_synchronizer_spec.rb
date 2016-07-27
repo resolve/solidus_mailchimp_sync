e968de921dfc8bb950a803f1dafd0c2c1d57e079
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-07-27 10:18:10 -0400

    user sync spec for email change

--- spec/synchronizers/user_synchronizer_spec.rb
+++ spec/synchronizers/user_synchronizer_spec.rb
@@ -28,7 +28,26 @@ describe SolidusMailchimpSync::UserSynchronizer, vcr: true do
       response = SolidusMailchimpSync::Mailchimp.ecommerce_request(:get, "/customers/#{SolidusMailchimpSync::UserSynchronizer.customer_id(user)}")
     end
     it "syncs" do
-      syncer.sync
+      response = syncer.sync
+      expect(response["id"]).to eq(SolidusMailchimpSync::UserSynchronizer.customer_id(user))
+      expect(response["email_address"]).to eq(user.email)
+    end
+
+    describe "email address changes" do
+      before do
+        @previous_id = syncer.mailchimp_id
+        user.email = "new-test-user-synchronizer@friendsoftheweb.com"
+        user.save!
+      end
+      it "syncs" do
+        response = syncer.sync
+        expect(response["id"]).to eq(SolidusMailchimpSync::UserSynchronizer.customer_id(user))
+        expect(response["email_address"]).to eq(user.email)
+
+        # It will have a new ID, it's a different mailchimp record, and we
+        # don't currently have anything to delete the old one.
+        expect(response["id"]).not_to eq(@previous_id)
+      end
     end
   end
 
