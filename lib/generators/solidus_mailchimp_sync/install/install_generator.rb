aa2aa98fcf77c4c16358a6297b3dcf88d00db2dd
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-07-28 12:58:21 -0400

    comment out migrations from generator, we have no migrations at present

--- lib/generators/solidus_mailchimp_sync/install/install_generator.rb
+++ lib/generators/solidus_mailchimp_sync/install/install_generator.rb
@@ -8,18 +8,20 @@ module SolidusMailchimpSync
         copy_file "config/initializers/solidus_mailchimp_sync.rb"
       end
 
-      def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=solidus_mailchimp_sync'
-      end
+      # No migrations at present
 
-      def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
-        if run_migrations
-          run 'bundle exec rake db:migrate'
-        else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
-        end
-      end
+      # def add_migrations
+      #   run 'bundle exec rake railties:install:migrations FROM=solidus_mailchimp_sync'
+      # end
+
+      # def run_migrations
+      #   run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+      #   if run_migrations
+      #     run 'bundle exec rake db:migrate'
+      #   else
+      #     puts 'Skipping rake db:migrate, don\'t forget to run it!'
+      #   end
+      # end
     end
   end
 end
