89612fd569655db1ef03558f6961373215c74fef
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-07-28 17:07:23 -0400

    cart modification auto_sync tests, including the really tricky case

--- spec/auto_sync/order_auto_sync_spec.rb
+++ spec/auto_sync/order_auto_sync_spec.rb
@@ -36,4 +36,39 @@ describe "Auto-sync on Spree::Order", clean: :truncation do
       order.complete!
     end
   end
+
+  describe "modify a cart" do
+    let!(:variant) { create(:variant) }
+    let!(:order) { create(:order, state: 'cart') }
+    it "syncs after add" do
+      # Not sure why it's being sent more than once, but we'll allow it.
+      expect(SolidusMailchimpSync::Mailchimp).to receive(:ecommerce_request) do |method, path, options|
+        expect(method).to eq(:post)
+        expect(path).to eq("/carts")
+      end.at_least(:once)
+
+      order.contents.add(variant)
+    end
+  end
+
+  describe "modify with update and exact same price in the end" do
+    let(:price) { 10.0.to_d }
+    let!(:variant1) { create(:variant, price: price) }
+    let!(:variant2) { create(:variant, price: price) }
+    let!(:order) { create(:order, state: 'cart').tap { |o| o.contents.add(variant1, 1) } }
+
+    # This is the really tricky one, that we're currently handling by syncing order
+    # on any line item after_commit, which also results in extra unneccesary syncs,
+    # in order to get this neccesary one.
+    it "still syncs" do
+      expect(SolidusMailchimpSync::Mailchimp).to receive(:ecommerce_request) do |method, path, options|
+        expect(method).to eq(:post)
+        expect(path).to eq("/carts")
+      end.at_least(:once)
+
+      order.contents.update_cart(line_items_attributes: [{id: order.line_items.first.id, quantity: 0}, { variant_id: variant2.id, quantity: 1 }])
+    end
+
+  end
+
 end
